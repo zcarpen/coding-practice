2816a6a4074b436413f665c41cce266e989ae670
zcarpen <zcarpen@gmail.com>
2022-05-17 08:39:14 -0500

    completed getDecimalValue

--- randomLeetCode/getDecimalValue.js
+++ randomLeetCode/getDecimalValue.js
@@ -1,3 +1,22 @@
-getDecimalValue = function (head) {
-  console.log(head)
-}
+var getDecimalValue = function (head) {
+  //     create array for linked list values
+  const linkedListValues = [];
+  //     create totalValue
+  let totalVal = 0;
+  // create counter
+  let counter = 0;
+  //     iterate over linked list and store values inside array
+  let curNode = head;
+  while (curNode) {
+    linkedListValues.push(curNode.val)
+    curNode = curNode.next;
+  }
+
+  //     iterate backward over linked list
+  for (let i = linkedListValues.length - 1; i >= 0; i--) {
+    totalVal += (2 ** counter) * linkedListValues[i]
+    counter++
+  }
+
+  return totalVal
+};
