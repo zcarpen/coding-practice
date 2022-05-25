ed91665c3c2c60a3710ef7bfcf9c916fdeaf2f1c
zcarpen <zcarpen@gmail.com>
2022-05-24 20:08:44 -0500

    longestPalindromeSubstring

--- udemyDataStructures/isPalindrome.js
+++ udemyDataStructures/isPalindrome.js
@@ -14,4 +14,6 @@ function isPalindrome(s) {
   return true;
 }
 
-export default isPalindrome;
+console.log(isPalindrome('A man, a plan, a canal: Panama'));
+
+// This problem first "sanitizes" all of the string by getting rid of all spaces and non-alphanumeric characters using regEx. Then we proceed by assigning pointers to the last character and first character... While looping through the string, if at any point the string at the index is different, this means it isn't a palindrome. However, if you can get to the point where the start pointer exceeds the end pointer without ever having a conflict, this means that the string would be a palindrome.
---
+++ udemyDataStructures/longestPalindromeSubstring.js
@@ -0,0 +1,22 @@
+function longestPalindrome(s) {
+  let longest = 1;
+  let startIndex = 0;
+
+  function expandAroundTheMiddle(left, right) {
+    while (s[left] === s[right] && left >= 0 && right < s.length) {
+      if (right - left + 1 > longest) {
+        startIndex = left;
+        longest = right - left + 1
+      }
+      left -= 1;
+      right += 1;
+    }
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    expandAroundTheMiddle(i - 1, i + 1);
+    expandAroundTheMiddle(i, i + 1);
+  }
+
+  return s.slice(startIndex, startIndex + longest)
+}
