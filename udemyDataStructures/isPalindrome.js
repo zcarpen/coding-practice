036163ee05c63a36a2000167228cbcdc84589c0d
zcarpen <zcarpen@gmail.com>
2022-05-24 19:31:16 -0500

    isPalindrome

---
+++ udemyDataStructures/isPalindrome.js
@@ -0,0 +1,15 @@
+function isPalindrome(s) {
+  s = s.toLowerCase().replace(/[\W_]/g, '')
+  console.log(s)
+  let end = s.length - 1;
+  let front = 0;
+  while (front < end) {
+    if (s[front] !== s[end]) {
+      return false
+    }
+
+    front++;
+    end--;
+  }
+  return true;
+}
